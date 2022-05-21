c37ef5fa041f9c66dd8cbb20ed0566d118865ead
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2022-05-20 22:18:29 -0400

    initial prototype

---
+++ ModDependencyAnalyzer/Program.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using OpenSoftware.DgmlTools;
+using OpenSoftware.DgmlTools.Builders;
+using OpenSoftware.DgmlTools.Model;
+using StardewModdingAPI;
+using StardewModdingAPI.Toolkit;
+using StardewModdingAPI.Toolkit.Framework.ModScanning;
+using StardewModdingAPI.Toolkit.Serialization.Models;
+
+namespace ModDependencyAnalyzer;
+
+/// <summary>The app entry point.</summary>
+internal static class Program
+{
+    /*********
+    ** Fields
+    *********/
+    /// <summary>The absolute path to the mods folder to analyze.</summary>
+    private const string ModsPath = @"C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Mods";
+
+    /// <summary>The absolute or relative path to the generated <c>.dgml</c> file.</summary>
+    private const string GeneratedFilePath = "mod-dependencies.dgml";
+
+    /// <summary>Whether to group mods by content pack, instead of adding a dependency link to their consuming mod.</summary>
+    private const bool GroupContentPacks = true;
+
+
+    /*********
+    ** Public methods
+    *********/
+    /// <summary>Run the app logic.</summary>
+    public static void Main()
+    {
+        // get mods
+        ModFolder[] mods = Program.GetInstalledMods(Program.ModsPath).ToArray();
+
+        // export .dgml file
+        DirectedGraph graph = Program.BuildDirectedGraph(mods, Program.GroupContentPacks);
+        Program.ExportToDgml(graph, Program.GeneratedFilePath);
+
+        // render image
+        //Program.ConvertDgmlToPng(Program.GeneratedFilePath);
+
+        Console.WriteLine($"Generated in {Environment.CurrentDirectory}.");
+    }
+
+
+    /*********
+    ** Private methods
+    *********/
+    /// <summary>Get the mods installed in the given <c>Mods</c> folder.</summary>
+    /// <param name="folderPath">The absolute path to the <c>Mods</c> folder to scan.</param>
+    private static IEnumerable<ModFolder> GetInstalledMods(string folderPath)
+    {
+        ModToolkit toolkit = new();
+        return toolkit
+            .GetModFolders(folderPath, useCaseInsensitiveFilePaths: true)
+            .Where(p => p.Type is not (ModType.Ignored or ModType.Invalid));
+    }
+
+    /// <summary>Build a directed graph for the given mods.</summary>
+    /// <param name="mods">The mods whose dependencies to visualize.</param>
+    /// <param name="groupContentPacks">Whether to group mods by content pack, instead of adding a dependency link to their consuming mod.</param>
+    private static DirectedGraph BuildDirectedGraph(ModFolder[] mods, bool groupContentPacks)
+    {
+        DgmlBuilder builder = new()
+        {
+            NodeBuilders = new[]
+            {
+                new NodeBuilder<ModFolder>(
+                    mod => new Node
+                    {
+                        Id = mod.Manifest!.UniqueID,
+                        Label = mod.DisplayName,
+                        Category = mod.Type.ToString()
+                    }
+                )
+            },
+            LinkBuilders = new[]
+            {
+                new LinksBuilder<ModFolder>(mod => GetDependencyLinks(mod, groupContentPacks))
+            }
+        };
+
+        return builder.Build(mods);
+    }
+
+    /// <summary>Export a directed graph to a <c>.dgml</c> file.</summary>
+    /// <param name="graph">The directed graph to export.</param>
+    /// <param name="filePath">The absolute path to the file to save.</param>
+    private static void ExportToDgml(DirectedGraph graph, string filePath)
+    {
+        using var writer = new StreamWriter(filePath);
+        var serializer = new XmlSerializer(graph.GetType());
+        serializer.Serialize(writer, graph);
+    }
+
+    /// <summary>Generate a PNG image from a given DGML file.</summary>
+    /// <param name="filePath">The DGML file path to convert.</param>
+    /// <remarks>The generated PNG will be in the same folder as the DGML file. This is experimental and doesn't handle large numbers of mods very well.</remarks>
+    private static void ConvertDgmlToPng(string filePath)
+    {
+        Process.Start(
+            new ProcessStartInfo
+            {
+                FileName = Path.Combine("lib", "DgmlImage", "DgmlImage.exe"),
+                ArgumentList = { filePath, /*"/zoom:4", "/width:10000", "/legend"*/ }, // usage docs: http://lovettsoftware.com/posts/dgmlimage
+                WorkingDirectory = Environment.CurrentDirectory
+            }
+        );
+    }
+
+    /// <summary>Get the DGML dependency links for a mod.</summary>
+    /// <param name="mod">The mod whose dependencies to fetch.</param>
+    /// <param name="groupContentPacks">Whether to group mods by content pack, instead of adding a dependency link to their consuming mod.</param>
+    private static IEnumerable<Link> GetDependencyLinks(ModFolder mod, bool groupContentPacks)
+    {
+        // get manifest
+        Manifest? manifest = mod.Manifest;
+        if (manifest is null)
+            yield break;
+
+        // content pack consumer
+        if (manifest.ContentPackFor?.UniqueID is not null)
+        {
+            if (groupContentPacks)
+            {
+                yield return new Link
+                {
+                    Source = manifest.ContentPackFor.UniqueID,
+                    Target = manifest.UniqueID,
+                    IsContainment = true
+                };
+            }
+            else
+            {
+                yield return new Link
+                {
+                    Source = manifest.UniqueID,
+                    Target = manifest.ContentPackFor.UniqueID
+                };
+            }
+        }
+
+        // required dependencies
+        foreach (IManifestDependency dependency in manifest.Dependencies)
+        {
+            if (!dependency.IsRequired)
+                continue;
+
+            yield return new Link
+            {
+                Source = manifest.UniqueID,
+                Target = dependency.UniqueID,
+                //Stroke = "Dashed"
+            };
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,71 @@
+﻿**Mod Dependency Analyzer** is a little utility which analyzes a Stardew Valley `Mods` folder and
+generates a directed graph of mod dependencies. The generated `.dgml` file can be opened in DGML
+viewers like Visual Studio to visualize the data.
+
+# Usage
+## First-time setup
+1. Install [Visual Studio](https://visualstudio.microsoft.com/) if needed (the Community edition is
+   free and has all the features you'll need).
+1. In Visual Studio, click _Tools > Get Tools and Features_.
+2. Click the _Individual Components_ tab and install _DGML editor_.
+
+## Generate a `.dgml` file
+1. Open the project in Visual Studio.
+2. In the `Program.cs` file, change the values under "Fields" if needed.
+3. Click _Debug > Start Without Debugging_ to launch the app.
+4. The file is generated at `bin/Debug/net6.0/mod-dependencies.dgml` by default.
+
+## View the dependency graph in Visual Studio
+First:
+1. Double-click the `.dgml` file to open it in Visual Studio's DGML editor.
+2. Click _Layout > Analyzers_ at the top of the DGML editor, and enable _Find Hubs Analyzer_ and
+   _Unreferenced Nodes Analyzer_.
+
+This will show a quick visual overview of the dependencies:
+> ![](docs/screenshots/dgml-viewer-overview.png)
+
+Specifically...
+* Content packs are grouped under the framework that loads them. (You can turn that off in the
+  settings.) Dependencies for each content pack are linked to the group for readability, but you
+  can click any content pack to see its specific dependencies.
+* Mods which are required by many other mods are highlighted in red (like Content Patcher).
+* Mods which aren't required by any other mods are highlighted in purple.
+
+For example, in the screenshot we can see that the entire TMXL Map Toolkit group is unreferenced.
+That means we could remove TMXL Map Toolkit and all its content packs, and all the other mods would
+still be loaded fine.
+
+You can also view specific subtrees. For example, which mods need the _Platonymous.Toolkit_
+framework (directly or indirectly)?
+1. Right-click _Platonymous.Toolkit_ in the graph, and choose _Select > Incoming Dependencies_.
+   This will highlight the _direct_ dependencies.
+2. Right-click the graph background, and choose _Select > Incoming Dependencies_ again to add the
+   incoming dependencies for all the selected mods. Repeat until no more mods get selected.
+3. Right-click the graph and choose _New Graph From Selection_.
+
+That gives us a graph of all the mods which need that mod installed:
+> ![](docs/screenshots/dgml-viewer-subtree.png)
+
+You can view similar trees like _Select > Outgoing Dependencies_ (mods needed by the selected mod),
+or _Select > Connected Subgraph_ (every direct or indirect dependency or dependent).
+
+## Export graph to PDF (Windows only)
+1. [Open the graph in Visual Studio](#view-the-dependency-graph-in-visual-studio).
+2. Click _Share > Save as Portable XPS_ at the top of the DGML editor.
+3. Open the exported file in [XPS Viewer](https://allthings.how/how-to-install-xps-viewer-on-windows-11/).
+4. Click _File > Print_, then choose _Microsoft Print to PDF_.
+
+## Export graph to image (Windows only)
+1. [Open the graph in Visual Studio](#view-the-dependency-graph-in-visual-studio).
+2. Click _Share > Save as Portable XPS_ at the top of the DGML editor.
+3. Use an online XPS to PNG tool like [Zamzar](https://www.zamzar.com/convert/xps-to-png/) to
+   convert it into an image.
+
+# For constributors
+## Bundled dependencies
+The `lib` folder includes two dependencies:
+
+* [DgmlImage](https://www.nuget.org/packages/DgmlImage) 1.2.0.1, extracted from the NuGet package
+  since NuGet no longer supports referencing bundled files.
+* [SMAPI](https://smapi.io/) mod toolkit 3.14.5, taken from the game's `smapi-internal` folder
+  after installing SMAPI.
